46366a3a84bfb92ba339da4c7bbcef83cd2d2edf
Vailery <leraliabik8@gmail.com>
2022-03-15 01:01:39 +0300

    feat: add left menu

---
+++ spotify/src/components/Menu/LibraryList.ts
@@ -0,0 +1,19 @@
+import { IMenuListState } from "./Menu";
+
+export const LibraryList: IMenuListState[] = [
+  {
+    id: 1,
+    img: "/assets/img/music-add.svg",
+    title: "Recently Added",
+  },
+  {
+    id: 2,
+    img: "/assets/img/favorite.svg",
+    title: "Favorite Songs",
+  },
+  {
+    id: 3,
+    img: "/assets/img/local.svg",
+    title: "Local Files",
+  },
+];
