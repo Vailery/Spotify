36c41ae2d0c9494c8590e83eaec2e118a41dc06f
Vailery <leraliabik8@gmail.com>
2022-03-15 01:01:59 +0300

    feat: add header

---
+++ spotify/src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
+
+export const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerContainer}>
+        <div className={styles.headerNav}>
+          <NavLink to="#" className={styles.headerButton}>
+            <img src="/assets/img/arrow.svg" alt="arrow" />
+          </NavLink>
+
+          <div className={styles.navigation}>
+            <span className={styles.prevLink}>Home</span>
+            <img src="/assets/img/small-arrow.svg" alt="arrow-revert" />
+            <span className={styles.currLink}>Popular Artist</span>
+          </div>
+        </div>
+
+        <div className={styles.headerAction}>
+          <div className={styles.searchContainer}>
+            <div className={styles.search}>
+              <button>
+                <img src="/assets/img/search.svg" alt="search" />
+              </button>
+
+              <input
+                type="text"
+                placeholder="Search music, artist, albums..."
+              />
+            </div>
+          </div>
+
+          <NavLink to="#" className={styles.headerButton}>
+            <img src="/assets/img/notification.svg" alt="arrow" />
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
