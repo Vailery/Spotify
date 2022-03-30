5522ecb85044c9caccb0dec253d3302aba46cdd6
Vailery <leraliabik8@gmail.com>
2022-03-30 21:04:08 +0300

    feat: add dropdown menu

--- spotify/src/components/RightMenu/RightMenu.tsx
+++ spotify/src/components/RightMenu/RightMenu.tsx
@@ -1,4 +1,5 @@
 import { IUserInfo } from "../../services/userApi";
+import { DropdownMenu } from "../DropdownMenu/DropdownMenu";
 import styles from "./RightMenu.module.css";
 
 interface IProps {
@@ -8,28 +9,7 @@ interface IProps {
 export const RightMenu = ({ user }: IProps) => {
   return (
     <div className={styles.rightMenu}>
-      <div className={styles.mainInfo}>
-        <div className={styles.userInfo}>
-          <div className={styles.avatar}>
-            <img src={user.avatarUrl} alt="avatar" />
-          </div>
-
-          <div className={styles.userStatus}>
-            <p className={styles.name}>{user.name}</p>
-
-            <p className={styles.status}>
-              Premium <span>•</span>{" "}
-              <img src="/assets/img/premium.svg" alt="avatar" />
-            </p>
-          </div>
-        </div>
-
-        <img
-          className={styles.more}
-          src="/assets/img/small-arrow.svg"
-          alt="arrow"
-        />
-      </div>
+      <DropdownMenu avatar={user.avatarUrl} name={user.name} />
     </div>
   );
 };
